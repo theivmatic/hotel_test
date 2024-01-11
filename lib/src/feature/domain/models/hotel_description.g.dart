4aff9edaec3de8d87e5ea17a6e89b47bf3ade49a
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-01-11 21:44:45 +0500

    generated file for hotel model

---
+++ lib/src/feature/domain/models/hotel_description.g.dart
@@ -0,0 +1,54 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'hotel_description.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+HotelDescriptionEntity _$HotelDescriptionEntityFromJson(
+        Map<String, dynamic> json) =>
+    HotelDescriptionEntity(
+      id: json['id'] as int?,
+      name: json['name'] as String?,
+      adress: json['adress'] as String?,
+      minimalPrice: json['minimal_price'] as int?,
+      priceForIt: json['price_for_it'] as String?,
+      rating: (json['rating'] as num?)?.toDouble(),
+      ratingName: json['rating_name'] as String?,
+      imageUrls: (json['image_urls'] as List<dynamic>?)
+          ?.map((e) => e as String)
+          .toList(),
+      aboutTheHotel: json['about_the_hotel'] == null
+          ? null
+          : AboutTheHotel.fromJson(
+              json['about_the_hotel'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$HotelDescriptionEntityToJson(
+        HotelDescriptionEntity instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'adress': instance.adress,
+      'minimal_price': instance.minimalPrice,
+      'price_for_it': instance.priceForIt,
+      'rating': instance.rating,
+      'rating_name': instance.ratingName,
+      'image_urls': instance.imageUrls,
+      'about_the_hotel': instance.aboutTheHotel,
+    };
+
+AboutTheHotel _$AboutTheHotelFromJson(Map<String, dynamic> json) =>
+    AboutTheHotel(
+      description: json['description'] as String?,
+      peculiarities: (json['peculiarities'] as List<dynamic>?)
+          ?.map((e) => e as String)
+          .toList(),
+    );
+
+Map<String, dynamic> _$AboutTheHotelToJson(AboutTheHotel instance) =>
+    <String, dynamic>{
+      'description': instance.description,
+      'peculiarities': instance.peculiarities,
+    };
