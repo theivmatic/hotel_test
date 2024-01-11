20126ee40be951ddf85563a3fddbb3aff8b78b52
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-01-11 20:19:15 +0500

    start

---
+++ README.md
@@ -0,0 +1,3 @@
+# hotel_test
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/hotel_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hotel_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
