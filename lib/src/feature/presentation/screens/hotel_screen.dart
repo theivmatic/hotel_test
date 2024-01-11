15581a78b70d42ae67fa6ed6a87d5f91cbff6137
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-01-11 23:35:42 +0500

    added custom caruosel slider to pubspec

--- lib/src/feature/presentation/screens/hotel_screen.dart
+++ lib/src/feature/presentation/screens/hotel_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hotel_test/src/feature/presentation/widgets/image_carousel_widget.dart';
 
 class HotelScreen extends StatelessWidget {
   const HotelScreen({super.key});
@@ -7,8 +8,21 @@ class HotelScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Отель'),
+        title: const Text(
+          'Отель',
+          style: TextStyle(color: Colors.black),
+        ),
         centerTitle: true,
+      ),
+      backgroundColor: const Color.fromRGBO(246, 246, 246, 1),
+      body: const Column(
+        children: [
+          //карусель
+          ImageCarouselWiidget(),
+          //рейтинг
+          //название
+          //стоимость
+        ],
       ),
     );
   }
